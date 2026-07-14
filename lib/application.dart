fc81a2cfb7e984f8941fa85bcd10a904651081be
makriq <141617622+makriq@users.noreply.github.com>
2026-07-14 08:55:04 +0300

    Убрать плашку подготовки встроенных узлов

--- lib/application.dart
+++ lib/application.dart
@@ -96,7 +96,6 @@ class ApplicationState extends ConsumerState<Application> {
                 appSettingProvider.select((state) => state.locale),
               );
               final themeProps = ref.watch(themeSettingProvider);
-              final runtimePreparing = !ref.watch(initProvider);
               return MaterialApp(
                 debugShowCheckedModeBanner: false,
                 navigatorKey: globalState.navigatorKey,
@@ -137,42 +136,7 @@ class ApplicationState extends ConsumerState<Application> {
                   // Reduce animation duration for snappier feel
                   visualDensity: VisualDensity.adaptivePlatformDensity,
                 ),
-                home: Stack(
-                  children: [
-                    child!,
-                    if (runtimePreparing)
-                      Positioned(
-                        left: 16,
-                        right: 16,
-                        bottom: 16,
-                        child: Material(
-                          elevation: 3,
-                          borderRadius: BorderRadius.circular(16),
-                          color: Theme.of(context)
-                              .colorScheme
-                              .surfaceContainerHigh,
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 12,
-                            ),
-                            child: Row(
-                              children: [
-                                SizedBox.square(
-                                  dimension: 20,
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2,
-                                  ),
-                                ),
-                                SizedBox(width: 12),
-                                Text('Подготовка встроенных узлов…'),
-                              ],
-                            ),
-                          ),
-                        ),
-                      ),
-                  ],
-                ),
+                home: child,
               );
             },
             child: const HomePage(),
